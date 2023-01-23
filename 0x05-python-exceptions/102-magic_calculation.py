3ad609c1465c5e004c16ad00287154a2c706f640
aktayo <113802719+aktayo@users.noreply.github.com>
2023-01-23 14:23:32 +0000

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,12 @@
-#!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def magic_calculation(a, b):
-    result = 0
+    varr = 0
     for i in range(1, 3):
         try:
-            if i > a:
+            if (i > a):
                 raise Exception('Too far')
             else:
-                result += a ** b / i
-        except:
-            result = b + a
+                varr += (a**b)/i
+        except Exception:
+            varr = (b + a)
             break
-    return (result)
+    return varr
