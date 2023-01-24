29aa6f89f1cd3916151d23eb00eefdbefdb95a3d
aktayo <113802719+aktayo@users.noreply.github.com>
2023-01-24 19:24:16 +0000

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-"""Defines a class Square by private"""
+"""Defines a square"""
 
 
 class Square:
-    """Represents a square"""
+    """represents a square."""
 
-    def __init__(self, size):
-        """Initializes a new square.
+    def __init__(self, size=0):
+        """initializes a new square.
         Args:
-        size(int): size of a new square.
+        size(int)=0: the optional size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
